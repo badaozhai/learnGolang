d2175c90a2f2a5a36e5244a86318af1ddb81cad2
badaozhai <89066216@qq.com>
2016-07-19 11:20:09 +0800

    format

--- day_02/box.go
+++ day_02/box.go
@@ -3,7 +3,7 @@ package day_02
 import "fmt"
 
 const (
-	WHITE=iota		//枚举计数器
+	WHITE = iota //枚举计数器
 	BLACK
 	BLUE
 	RED
@@ -12,23 +12,23 @@ const (
 
 type Color byte
 
-type Box struct  {
-	width,height,depth float64
-	color Color
-
+type Box struct {
+	width, height, depth float64
+	color                Color
 }
+
 // a slince of box
 type BoxList []Box
 
 //函数
-func (b Box)Volume()  float64{
-	return b.height*b.width*b.depth
-}
-// 接收者是一个Box的指针
-func (b *Box)SetColor(c Color){
-	b.color = c
+func (b Box) Volume() float64 {
+	return b.height * b.width * b.depth
 }
 
+// 接收者是一个Box的指针
+func (b *Box) SetColor(c Color) {
+	b.color = c
+}
 
 func (bl BoxList) BiggestColor() Color {
 	v := 0.00
@@ -48,11 +48,11 @@ func (bl BoxList) PaintItBlack() {
 }
 
 func (c Color) String() string {
-	strings := []string {"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
+	strings := []string{"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
 	return strings[c]
 }
-func BoxDemo(){
-	boxes := BoxList {
+func BoxDemo() {
+	boxes := BoxList{
 		Box{4, 4, 4, RED},
 		Box{10, 10, 1, YELLOW},
 		Box{1, 1, 20, BLACK},
@@ -63,7 +63,7 @@ func BoxDemo(){
 
 	fmt.Printf("We have %d boxes in our set\n", len(boxes))
 	fmt.Println("The volume of the first one is", boxes[0].Volume(), "cm³")
-	fmt.Println("The color of the last one is",boxes[len(boxes)-1].color.String())
+	fmt.Println("The color of the last one is", boxes[len(boxes)-1].color.String())
 	fmt.Println("The biggest one is", boxes.BiggestColor().String())
 
 	fmt.Println("Let's paint them all black")
@@ -71,5 +71,5 @@ func BoxDemo(){
 	fmt.Println("The color of the second one is", boxes[1].color.String())
 
 	fmt.Println("Obviously, now, the biggest one is", boxes.BiggestColor().String())
-	fmt.Println("now,ths boxlist is,",boxes)
+	fmt.Println("now,ths boxlist is,", boxes)
 }
